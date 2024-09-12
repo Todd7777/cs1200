dec9b7f44621058f26df1e5c80d0d3a42415c84a
Todd7777 <144302873+Todd7777@users.noreply.github.com>
2024-09-11 22:40:14 -0400

    Update ps0.py

--- fall2024/psets/ps0/ps0.py
+++ fall2024/psets/ps0/ps0.py
@@ -5,52 +5,53 @@
 #################
 
 
-#
-# Setup
-#
 class BinaryTree:
     def __init__(self, root):
         """
         :param root: the root of the binary tree
         """
-        self.root: BTvertex = root
- 
+        self.root = root
+
 class BTvertex:
     def __init__(self, key):
         """
-        :param: the key associated with the vertex of the binary tree
+        :param key: the key associated with the vertex of the binary tree
         """
-        self.parent: BTvertex = None
-        self.left: BTvertex = None
-        self.right: BTvertex = None
-        self.key: int = key
-        self.size: int = None
+        self.parent = None
+        self.left = None
+        self.right = None
+        self.key = int(key)
+        self.size = None
 
-
-#
-# Problem 1a
-#
-
-# Input: BTvertex v, the root of a BinaryTree of size n
-# Output: Up to you
-# Side effect: sets the size of each vertex n in the
-# ... tree rooted at vertex v to the size of that subtree
-# Runtime: O(n)
 def calculate_sizes(v):
-    # Your code goes here
-    pass 
+    """
+    Recursively calculates the sizes of all the subtrees rooted at descendants of vertex v.
+    Each vertex v in the tree will have v.size set to the size of the subtree rooted at v.
 
+    :param v: BTvertex, the root of a subtree
+    """
+    if v is None:
+        return 0
+    left_size = calculate_sizes(v.left)
+    right_size = calculate_sizes(v.right)
+    v.size = 1 + left_size + right_size
+    return v.size
 
-#
-# Problem 1c
-#
-
-# Input: a positive integer t, 
-# ...BTvertex v, the root of a BinaryTree of size n >= 1
-# Output: BTvertex, descendent of v such that its size is between 
-# ... t and 2t (inclusive)
-# Runtime: O(h) 
 
 def FindDescendantOfSize(t, v):
-    # Your code goes here 
-    pass 
+    current = v
+    while current is not None:
+        if t <= current.size <= 2 * t:
+            return current
+        # Decide which child to traverse based on the size
+        if current.left and t <= current.left.size <= 2 * t:
+            current = current.left
+        elif current.right and t <= current.right.size <= 2 * t:
+            current = current.right
+        elif current.left and current.left.size > 2 * t:
+            current = current.left
+        elif current.right and current.right.size > 2 * t:
+            current = current.right
+        else:
+            break  # No suitable child to explore, and current is not suitable either
+    return None  # If no such vertex is found
